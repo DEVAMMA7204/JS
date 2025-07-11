efcfc6b3126bb7962227595557c3a19d237f079d
DEVAMMA7204 <malibiradardevamma@gmail.com>
2025-07-11 08:12:09 +0000

    datatypes

---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//premitive datatype
+// number => 2 to power 53
+// bigint (if number big)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => (value didnot assigned)
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
